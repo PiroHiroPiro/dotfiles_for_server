8905963b578f9532dfebc615be4dc259033f7204
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-12-09 18:25:53 +0900

    :+1: add curl option

--- tmux/.config/tmux/ip.sh
+++ tmux/.config/tmux/ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # https://qiita.com/ksugawara61/items/887ddd1792d7d5dabb25
-publicip=$(curl inet-ip.info)
+publicip=$(curl -s inet-ip.info)
 # https://moomindani.wordpress.com/2014/09/17/linux-command-ip-address/
 privateip=$(hostname -I | cut -f1 -d' ')
 
