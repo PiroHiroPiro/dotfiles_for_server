1b8c0bf553a7dc935906366e60d48d0cbd8126ce
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-12-09 18:23:16 +0900

    :+1: show publicip

--- tmux/.config/tmux/ip.sh
+++ tmux/.config/tmux/ip.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
+# https://qiita.com/ksugawara61/items/887ddd1792d7d5dabb25
+publicip=$(curl inet-ip.info)
 # https://moomindani.wordpress.com/2014/09/17/linux-command-ip-address/
-hostname -I | cut -f1 -d' '
+privateip=$(hostname -I | cut -f1 -d' ')
+
+echo "${publicip}(${privateip})"
